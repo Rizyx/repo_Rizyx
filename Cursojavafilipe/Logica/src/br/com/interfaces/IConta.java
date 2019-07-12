96ae01597c1b9e713be2239444f0416d8ab837b6
Noturno <filipebcordeirobr@hotmail.com>
2019-07-12 20:03:16 -0300

    bora

---
+++ Cursojavafilipe/Logica/src/br/com/interfaces/IConta.java
@@ -0,0 +1,6 @@
+package br.com.interfaces;
+
+public interface IConta {
+
+
+}
---
+++ Cursojavafilipe/Logica/src/br/com/interfaces/TesteConta.java
@@ -0,0 +1,21 @@
+package br.com.interfaces;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente();
+			cc.setLimite(1500);
+			cc.setNumconta(1);
+			cc.setSaldo(4875.75);
+		ContaPoupanca cp = new ContaPoupanca();
+			cp.setLimite(1000);
+			cp.setNumconta(1);
+			cp.setSaldo(24655.10);
+		System.out.println(cp.getLimite());
+		cc.deposito(6500);
+		System.out.println(cc.getSaldo());
+		cc.saque(9000);
+		System.out.println(cc.getSaldo());		
+	}
+
+}
